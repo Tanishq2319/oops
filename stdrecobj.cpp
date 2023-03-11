ac9a2b81327cb5baef4b2dc91b7f995c605f08ec
Tanishq <104089429+Tanishq2319@users.noreply.github.com>
2023-03-11 11:51:01 +0530

    Create stdrecobj.cpp

---
+++ stdrecobj.cpp
@@ -0,0 +1,23 @@
+class student
+{
+ int rno;
+char name[10];
+public:
+ void getdata()
+{
+Cout<<”enter rno,name”;
+cin>> name>>rno;
+}
+Void putdata
+{ 
+cout<<”Name=”
+cout<< name
+cout <<”Rno=”
+cout<<rno;
+ };
+ void main()
+{ 
+student s1;
+s1.getdata();
+s1.putdata();
+}
